5dae402289dbfbe2371ee1e3668560f73fe328b3
yourcomrade <MinhXecole@gmail.com>
2022-10-26 23:16:41 +0200

    Add BIT

--- src/opcode.c
+++ src/opcode.c
@@ -18,5 +18,15 @@ opcode my_opcode[]={
         {"Arithmetic shift left",0x0e,"ASL",adr,ASL},
         {"Branch on plus",0x10,"BPL",none,BPL},
         {"Or with accumulator",0x11,"ORA",pos_ind_Y,ORA},
+        {"Or with accumulator",0x15,"ORA",zp_X,ORA},
+        {"Arithmetic shift left",0x16,"ASL",zp_X,ASL},
+        {"Clear carry flag",0x18,"CLC",none,CLC},
+        {"Or with accumulator",0x19,"ORA",adr_Y,ORA},
+        {"Or with accumulator",0x1d,"ORA",adr_X,ORA},
+        {"Arithmetic shift left",0x1e,"ASL",adr_X,ASL},
+        {"Jump subroutine",0x20,"JSR",none,JSR},
+        {"And with accumulator",0x21,"AND",pre_ind_X,AND},
+        {"Test bit",0x24,"BIT",zp,BIT},
+        {}
 };
 #endif
